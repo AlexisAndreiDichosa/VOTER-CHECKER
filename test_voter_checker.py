c676f7f62cebf9284a00c4d8c22ff28ae6e70bb5
Yume <alexisandreidichosa24@gmail.com>
2025-05-16 20:53:19 +0800

    update

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -8,13 +8,13 @@ class TestVoterEligibility(unittest.TestCase):
         self.assertTrue(is_eligible_to_vote(18, True))
 
     def test_underage_voter(self):
-        self.assertTrue(is_eligible_to_vote(17, True))
+        self.assertTrue(is_eligible_to_vote(17, False))
 
     def test_no_citizen_voter(self):
         self.assertTrue(is_eligible_to_vote(20, True))
 
     def test_underage_non_citizen(self):
-        self.assertTrue(is_eligible_to_vote(16, True))
+        self.assertTrue(is_eligible_to_vote(16, False))
 
     def test_exactly_18_and_non_citizen(self):
         self.assertTrue(is_eligible_to_vote(18, True))
