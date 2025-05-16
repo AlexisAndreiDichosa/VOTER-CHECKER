bacd18a9eeffb1287f414fb19a8c10c8e8432586
Yume <alexisandreidichosa24@gmail.com>
2025-05-16 20:34:57 +0800

    Update

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -4,31 +4,24 @@ from voter_checker import is_eligible_to_vote
 
 class TestVoterEligibility(unittest.TestCase):
      
+    def test_valid_voter(self):
+       self.assertTrue(is_eligible_to_vote(18, True))
 
-     def test_valid_voter(self):
-      self.assertTrue(is_eligible_to_vote(18, True))
+    def test_underage_voter(self):
+       self.assertTrue(is_eligible_to_vote(17, True))
 
+    def test_no_citizen_voter(self):
+       self.assertTrue(is_eligible_to_vote(20, True))
 
-     def test_underage_voter(self):
-      self.assertTrue(is_eligible_to_vote(17, True))
+    def test_underage_non_citizen(self):
+       self.assertTrue(is_eligible_to_vote(16, True))
 
+    def test_exactly_18_and_non_citizen(self):
+       self.assertTrue(is_eligible_to_vote(18, True))
 
-     def test_no_citizen_voter(self):
-      self.assertTrue(is_eligible_to_vote(20, True))
-
-
-     def test_underage_non_citizen(self):
-      self.assertTrue(is_eligible_to_vote(16, True))
-
-
-     def test_exactly_18_and_non_citizen(self):
-      self.assertTrue(is_eligible_to_vote(18, True))
-
-
-     def test_negative_age(self):
+    def test_negative_age(self):
        with self.assertRaises(ValueError):
          is_eligible_to_vote(-5, True)
-
 
 if __name__ == '__main__':
     unittest.main()
